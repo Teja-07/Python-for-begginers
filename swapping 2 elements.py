2fcb9f546d4f9821185a8fdc0b5fd8012d61a5ec
Teja chinitha <82213085+Teja-07@users.noreply.github.com>
2021-06-15 08:58:32 +0530

    Add files via upload

---
+++ swapping 2 elements.py
@@ -0,0 +1,12 @@
+'''
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+'''
+a=10
+b=5
+a,b=b,a
+print("Elements after swapping:",a,b)
